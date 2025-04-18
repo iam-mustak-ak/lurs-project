c0124ffa753106321c6711e3765b25d127c23c94
Mustak Ahmed Khan <mustakahmedkhan0@gmail.com>
2025-04-18 18:47:08 +0600

    update meta data

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,34 @@ const inter = Inter({
 });
 
 export const metadata = {
-    title: "LURS - Research",
-    description: "Some description will be added leter",
+    title: "LURS - Leading University Research Society",
+    description:
+        "The Leading University Research Society (LURS) cordially invites students, researchers, academics, and professionals from diverse disciplines to submit original research for presentation at the “2nd Leading University Research Conference 2025” (2nd LURC 2025)",
+    icon: {
+        icon: "/favicon.ico",
+        apple: "/apple-touch-icon.png",
+    },
+    openGraph: {
+        title: "LURS - Leading University Research Society",
+        description:
+            "The Leading University Research Society (LURS) cordially invites students, researchers, academics, and professionals from diverse disciplines to submit original research for presentation at the “2nd Leading University Research Conference 2025” (2nd LURC 2025)",
+        url: "https://lurs-project.vercel.app/",
+        siteName: "LURS - Leading University Research Society",
+        images: [
+            {
+                url: "/g_1.jpg",
+                width: 1200,
+                height: 630,
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "LURS - Leading University Research Society",
+        description:
+            "The Leading University Research Society (LURS) cordially invites students, researchers, academics, and professionals from diverse disciplines to submit original research for presentation at the “2nd Leading University Research Conference 2025” (2nd LURC 2025)",
+        images: ["/g_1.jpg"],
+    },
 };
 
 export default function RootLayout({ children }) {
