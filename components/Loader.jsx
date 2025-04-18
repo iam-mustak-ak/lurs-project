bf31439c49aa18f466866847efb5619ecd23577e
Mustak Ahmed Khan <mustakahmedkhan0@gmail.com>
2025-04-18 21:51:05 +0600

    issue fixed

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,15 +1,7 @@
-"use client";
-
-import { InfinitySpin } from "react-loader-spinner";
-
 const Loader = () => {
     return (
         <div className="flex items-center justify-center h-screen w-full">
-            <InfinitySpin
-                width="200"
-                color="#4fa94d"
-                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            />
+            <p className="font-bold">Loading...</p>
         </div>
     );
 };
