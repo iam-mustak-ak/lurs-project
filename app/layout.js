1045ea7f3b3da7faf515dd34fe59b0ab69f8d7cb
Mustak Ahmed Khan <mustakahmedkhan0@gmail.com>
2025-04-20 01:15:53 +0600

    Added SEO

--- app/layout.js
+++ app/layout.js
@@ -7,18 +7,51 @@ const inter = Inter({
 });
 
 export const metadata = {
-    title: "LURS - Leading University Research Society",
+    metadataBase: new URL("https://www.lurc2025.com"),
+    title: "LURC - Leading University Research Conference 2025",
+    keywords: [
+        "LURS",
+        "LURC",
+        "Leading University Research Society",
+        "Leading University Research Conference 2025",
+        "LURC 2025",
+        "Leading University",
+        "Leading University Research",
+        "Research Conference",
+        "Research Society",
+        "Research",
+        "Conference",
+        "Academic Conference",
+        "Research Presentation",
+        "Research Publication",
+        "Research Paper",
+        "Research Symposium",
+        "Research Workshop",
+        "Research Forum",
+        "Research Networking",
+        "Research Collaboration",
+        "Research Community",
+        "2nd LURC 2025",
+    ],
+    authors: [
+        {
+            name: "LURS",
+            url: "https://www.lurc2025.com",
+        },
+    ],
+    creator: "LURS",
+    publisher: "LURS",
     description:
         "The Leading University Research Society (LURS) cordially invites students, researchers, academics, and professionals from diverse disciplines to submit original research for presentation at the “2nd Leading University Research Conference 2025” (2nd LURC 2025)",
     icon: {
         apple: "/apple-touch-icon.png",
     },
     openGraph: {
-        title: "LURS - Leading University Research Society",
+        title: "LURC - Leading University Research Conference 2025",
         description:
             "The Leading University Research Society (LURS) cordially invites students, researchers, academics, and professionals from diverse disciplines to submit original research for presentation at the “2nd Leading University Research Conference 2025” (2nd LURC 2025)",
-        url: "https://lurs-lurc.vercel.app/",
-        siteName: "LURS - Leading University Research Society",
+        url: "https://www.lurc2025.com",
+        siteName: "LURC - Leading University Research Conference 2025",
         images: [
             {
                 url: "/g_1.jpg",
@@ -28,8 +61,8 @@ export const metadata = {
         ],
     },
     twitter: {
-        card: "The Leading University Research Society",
-        title: "LURS - Leading University Research Society",
+        card: "Leading University Research Conference 2025",
+        title: "LURC - Leading University Research Conference 2025",
         description:
             "The Leading University Research Society (LURS) cordially invites students, researchers, academics, and professionals from diverse disciplines to submit original research for presentation at the “2nd Leading University Research Conference 2025” (2nd LURC 2025)",
         images: ["/g_1.jpg"],
---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: "https://www.lurc2025.com/sitemap.xml",
+    };
+}
